ac41d337d39432d30dba96bac0109b8d54a27f4f
MisterM <510249@student.saxion.nl>
2022-11-23 10:37:44 +0100

    Added primitive startbutton

---
+++ Assets/StartButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+
+
+
+    private void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+    }
+}
