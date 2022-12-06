9cb28b3a7ca4b696e32fde07a021ef491dba50db
Koen H <49477924+Koen-H@users.noreply.github.com>
2022-12-06 13:48:13 +0100

    Player countdown. press R

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -7,9 +7,21 @@ public class PlayerData : MonoBehaviour
     int playerNumber = 0;
     string playerName = "PlayerName";
     GameObject playerGameObject;
+    Canvas playerCanvas;
 
     private void Awake()
     {
         playerGameObject = this.gameObject;
+        this.playerCanvas = GetComponentInChildren<Canvas>();
+    }
+
+    public void SetName(string newName)
+    {
+        playerName = newName;
+    }
+
+    public string GetName()
+    {
+        return playerName;
     }
 }
